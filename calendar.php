e2712555d1bce08d69332cbfa825eec540692661
ChrisMedland <chris@medland.net>
2014-04-14 11:31:38 +0100

    Recurrence data

--- calendar.php
+++ calendar.php
@@ -49,50 +49,34 @@ if(isset($_POST['action']))
 	$until = $_POST["until"];
 	$recurrence = $_POST["recurrence"];
 
+
+
 	$event = new Google_Event(); 
 	$event->setSummary($title);
 	$event->setDescription($desc);
 	$event->setLocation($locat);
 	$start = new Google_EventDateTime(); 
 	$start->setDateTime($from);
+	$start->setTimeZone('Europe/London')
 	$event->setStart($start);
 	$end = new Google_EventDateTime(); 
 	$end->setDateTime($until);
+	$end->setTimeZone('Europe/London')
 	$event->setEnd($end);
+	$event-> setRecurrence(array('RRULE:FREQ=$frequency;UNTIL=$EndRec'))
+
+
 	$attendee1 = new Google_EventAttendee(); 
 	$attendee1->setEmail('christophermedland@googlemail.com'); 
 
 
-	$createdEvent = $cal->events->insert('primary', $event);
+	$recurringEvent = $service->events->insert('primary', $event);
 
-	echo $createdEvent->getId(); 
+	echo $recurringEvent->getId(); 
 }
 }
 
-print "<h1>Add Item To Calendar</h1>";
 
-print "<div style='width:100%;height:100%;'><div style='width:550px;height:320px;text-align:center;margin:200px auto;background-color:#f0ffff;border:1px solid black;'><div style='padding:50px;'><table>";
-print "<form method='POST'>";
-print "<tr><td>Title: </td><td><input type='text' name='title' id='title'></td></tr>";
-print "<tr><td>Desc: </td><td><input type='text' name='desc' id='desc'></td></tr>";
-print "<tr><td>Location: </td><td><input type='text' name='locat' id='locat'></td></tr>";
-print "<tr><td colspan='2'>Date Format: YYYY-MM-DDTHH:MM:SS(.000)-00:00 (.000 = Optional)</td></tr>";
-print "<tr><td>From: </td><td><input type='text' name='from' id='from'></td></tr>";
-print "<tr><td>To: </td><td><input type='text' name='until' id='until'></td></tr>";
-print "<tr><td> <input type='checkbox'></td><td>repeat every: </td><td><input type= 'text' name='recurrence' id='recurrence></td></tr>";
-print "<tr><td>Select Calendar: </td><td><select name='calID' id='chooseCal'>";
-for($i=0; $i < count($calList['items']); $i++)
-{
-print "<option value=".$calList['items'][$i]['id']."'>".$calList['items'][$i]['summary']."</option>";
-
-}
-print "</select></br></td></tr>";
-print "<input type='hidden' name='action' value='addCalEvent'>";
-print "<tr><td colspan='2'><div style='text-align:right;'><input type='submit' value='Submit'></div></td></tr>";
-print "</form>";
-print "</table></div></div></div>";
-
-print "<h1>Calendar List</h1><pre>" . print_r($calList, true) . "</pre>";
 
 $_SESSION['token'] = $client->getAccessToken();
 }
